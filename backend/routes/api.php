90e3d43ea4477db52623be2b6563e7752427f689
arup-paul <aruppaulctg@gmail.com>
2023-08-30 10:15:06 +0600

    register api

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -16,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
     Route::post('login', [AuthController::class, 'login']);
+    Route::post('register', [AuthController::class, 'register']);
     Route::group(['middleware' => ['api']], function() {
         Route::post('logout', [AuthController::class, 'logout']);
         Route::post('refresh', [AuthController::class, 'refresh']);
